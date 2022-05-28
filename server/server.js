2ef2e3814e9871086a186c6d48f06565f97470c5
Alex Contreras <alex.code29@gmail.com>
2022-05-28 16:58:17 -0500

    added thought to homepage

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 // Import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas')
@@ -11,13 +13,22 @@ const PORT = process.env.PORT || 3001;
 // create a new apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 })
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // create a new instance of an apollo servcer with GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
