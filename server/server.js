c6ba7a66d64722078fc634c055a7d68c03999cd3
Alex Contreras <alex.code29@gmail.com>
2022-09-15 03:17:10 -0500

    feat: add react router and setup single page and profile layout

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   };
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, '../client/build/index.html'))
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
 
 
